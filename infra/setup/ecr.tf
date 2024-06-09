e881995f2e0bf92322e07f75bef5546bb7cc427f
mostafasookar <mostafasookar@hotmail.com>
2024-06-09 22:52:20 +0200

    adding ECR

---
+++ infra/setup/ecr.tf
@@ -0,0 +1,26 @@
+
+##############################################
+# Create ECR repos for storing Docker images #
+##############################################
+
+resource "aws_ecr_repository" "app" {
+  name                 = "ecr-app-api-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # NOTE: Update to true for real deployments.
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "proxy" {
+  name                 = "ecr-app-api-proxy"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # NOTE: Update to true for real deployments.
+    scan_on_push = false
+  }
+}
